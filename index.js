e74fe7f2a98264b85195d24cc63df56118c40b49
Shahnawazatr <Shahnawaz@skills-agency.com>
2022-06-28 17:29:53 +0530

    node-js-resize-image

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const multer = require('multer');
+const path = require('path');
+const sharp = require('sharp');
+const fs = require('fs');
+const app = express();
+   
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, 'uploads/');
+    },
+   
+    filename: function(req, file, cb) {
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+    }
+});
+   
+var upload = multer({ storage: storage })
+   
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+   
+app.post('/', upload.single('image'),async (req, res) => {
+       const { filename: image } = req.file;
+       
+       await sharp(req.file.path)
+        .resize(200, 200)
+        .jpeg({ quality: 90 })
+        .toFile(
+            path.resolve(req.file.destination,'resized',image)
+        )
+        
+
+        // unlink the original file from uploads
+        fs.unlinkSync(req.file.path);
+       
+       res.redirect('/');
+});
+   
+app.listen(3600);
